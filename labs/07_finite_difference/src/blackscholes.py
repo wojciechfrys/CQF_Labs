f96ded31a14c330de200d86c7c65ae74a0b7e76a
Wojciech Frys <frys.wojciech@gmail.com>
2023-09-09 13:28:48 +0000

    Labs 6 & 7

---
+++ labs/07_finite_difference/src/blackscholes.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Black Scholes Option Pricing
+#
+# Jay Patel, CQF
+#
+# June 2023
+
+# In[2]:
+
+
+# Plotting
+import matplotlib.pyplot as plt
+
+# Importing libraries
+import pandas as pd
+import yfinance as yf
+from numpy import *
+from scipy.stats import norm
+from tabulate import tabulate
+
+# Set max row to 300
+pd.set_option("display.max_rows", 300)
+
+
+# In[24]:
+
+
+class BS:
+
+    """
+    This is a class for Options contract for pricing European options on stocks␣
+    ,→without dividends.
+    Attributes:
+    spot : int or float
+    strike : int or float
+    rate : float
+    dte : int or float [days to expiration in number of years]
+    volatility : float
+    """
+
+    def __init__(self, spot, strike, rate, dte, volatility):
+        # Spot Price
+        self.spot = spot
+
+        # Option Strike
+        self.strike = strike
+
+        # Interest Rate
+        self.rate = rate
+
+        # Days To Expiration
+        self.dte = dte
+
+        # Volaitlity
+        self.volatility = volatility
+
+        # Utility
+        self._a_ = self.volatility * self.dte**0.5
+
+        if self.strike == 0:
+            raise ZeroDivisionError("The strike price cannot be zero")
+        else:
+            self._d1_ = (
+                log(self.spot / self.strike) + (self.rate + (self.volatility**2) / 2) * self.dte
+            ) / self._a_
+
+        self._d2_ = self._d1_ - self._a_
+        self._b_ = e ** -(self.rate * self.dte)
+        # The __dict__ attribute
+        """
+        Contains all the attributes defined for the object itself. It maps the attribute name to its value.
+        """
+
+        for i in [
+            "callPrice",
+            "putPrice",
+            "callDelta",
+            "putDelta",
+            "callTheta",
+            "putTheta",
+            "callRho",
+            "putRho",
+            "vega",
+            "gamma",
+        ]:
+            self.__dict__[i] = None
+
+        [self.callPrice, self.putPrice] = self._price
+        [self.callDelta, self.putDelta] = self._delta
+        [self.callTheta, self.putTheta] = self._theta
+        [self.callRho, self.putRho] = self._rho
+        self.vega = self._vega
+        self.gamma = self._gamma
+
+    # Option Price
+    @property
+    def _price(self):
+        """Returns the option price: [Call price, Put price]"""
+
+        if self.volatility == 0 or self.dte == 0:
+            call = maximum(0.0, self.spot - self.strike)
+            put = maximum(0.0, self.strike - self.spot)
+        else:
+            call = self.spot * norm.cdf(self._d1_) - self.strike * e ** (
+                -self.rate * self.dte
+            ) * norm.cdf(self._d2_)
+            put = self.strike * e ** (-self.rate * self.dte) * norm.cdf(
+                -self._d2_
+            ) - self.spot * norm.cdf(-self._d1_)
+        return [call, put]
+
+    # Option Delta
+    @property
+    def _delta(self):
+        """Returns the option delta: [Call delta, Put delta]"""
+        if self.volatility == 0 or self.dte == 0:
+            call = 1.0 if self.spot > self.strike else 0.0
+            put = -1.0 if self.spot < self.strike else 0.0
+        else:
+            call = norm.cdf(self._d1_)
+            put = -norm.cdf(-self._d1_)
+        return [call, put]
+
+    # Option Gamma
+    @property
+    def _gamma(self):
+        """Returns the option gamma"""
+        return norm.pdf(self._d1_) / (self.spot * self._a_)
+
+    # Option Vega
+    @property
+    def _vega(self):
+        """Returns the option vega"""
+        if self.volatility == 0 or self.dte == 0:
+            return 0.0
+        else:
+            return self.spot * norm.pdf(self._d1_) * self.dte**0.5 / 100
+
+    # Option Theta
+    @property
+    def _theta(self):
+        """Returns the option theta: [Call theta, Put theta]"""
+        call = -self.spot * norm.pdf(self._d1_) * self.volatility / (
+            2 * self.dte**0.5
+        ) - self.rate * self.strike * self._b_ * norm.cdf(self._d2_)
+        put = -self.spot * norm.pdf(self._d1_) * self.volatility / (
+            2 * self.dte**0.5
+        ) + self.rate * self.strike * self._b_ * norm.cdf(-self._d2_)
+        return [call / 365, put / 365]
+
+    # Option Rho
+    @property
+    def _rho(self):
+        """Returns the option rho: [Call rho, Put rho]"""
+        call = self.strike * self.dte * self._b_ * norm.cdf(self._d2_) / 100
+        put = -self.strike * self.dte * self._b_ * norm.cdf(-self._d2_) / 100
+        return [call, put]
---
+++ labs/08/optionpricer2.py
@@ -0,0 +1,169 @@
+# Importing libraries
+from numpy import *
+from scipy.stats import norm
+
+
+class BS:
+    
+    """
+    This is a class for Options contract for pricing European options on stocks without dividends.
+    
+    Attributes: 
+        spot          : int or float
+        strike        : int or float 
+        rate          : float
+        dte           : int or float [days to expiration in number of years]
+        volatility    : float
+    """    
+    
+    def __init__(self, spot, strike, rate, dte, volatility, callprice=None, putprice=None):
+        
+        # Spot Price
+        self.spot = spot
+        
+        # Option Strike
+        self.strike = strike
+        
+        # Interest Rate
+        self.rate = rate
+        
+        # Days To Expiration
+        self.dte = dte
+        
+        # Volaitlity
+        self.volatility = volatility
+        
+        # Callprice > mkt price of call
+        self.callprice = callprice
+        
+        # Putprice > mkt price of put
+        self.putprice = putprice
+       
+        # Utility 
+        self._a_ = self.volatility * self.dte**0.5
+        
+        if self.strike == 0:
+            raise ZeroDivisionError('The strike price cannot be zero')
+        else:
+            self._d1_ = (log(self.spot / self.strike) + \
+                     (self.rate + (self.volatility**2) / 2) * self.dte) / self._a_
+        
+        self._d2_ = self._d1_ - self._a_
+        
+        self._b_ = e**-(self.rate * self.dte)
+        
+        
+        # The __dict__ attribute
+        '''
+        Contains all the attributes defined for the object itself. It maps the attribute name to its value.
+        '''
+        for i in ['callPrice', 'putPrice', 'callDelta', 'putDelta', 'callTheta', 'putTheta', \
+                  'callRho', 'putRho', 'vega', 'gamma', 'impvol']:
+            self.__dict__[i] = None
+        
+        [self.callPrice, self.putPrice] = self._price
+        [self.callDelta, self.putDelta] = self._delta
+        [self.callTheta, self.putTheta] = self._theta
+        [self.callRho, self.putRho] = self._rho
+        self.vega = self._vega
+        self.gamma = self._gamma
+        self.impvol = self._impvol
+        
+    
+    # Option Price
+    @property
+    def _price(self):
+        '''Returns the option price: [Call price, Put price]'''
+
+        if self.volatility == 0 or self.dte == 0:
+            call = maximum(0.0, self.spot - self.strike)
+            put = maximum(0.0, self.strike - self.spot)
+        else:
+            call = self.spot * norm.cdf(self._d1_) - self.strike * e**(-self.rate * \
+                                                                       self.dte) * norm.cdf(self._d2_)
+
+            put = self.strike * e**(-self.rate * self.dte) * norm.cdf(-self._d2_) - \
+                                                                        self.spot * norm.cdf(-self._d1_)
+        return [call, put]
+
+    # Option Delta
+    @property
+    def _delta(self):
+        '''Returns the option delta: [Call delta, Put delta]'''
+
+        if self.volatility == 0 or self.dte == 0:
+            call = 1.0 if self.spot > self.strike else 0.0
+            put = -1.0 if self.spot < self.strike else 0.0
+        else:
+            call = norm.cdf(self._d1_)
+            put = -norm.cdf(-self._d1_)
+        return [call, put]
+
+    # Option Gamma
+    @property
+    def _gamma(self):
+        '''Returns the option gamma'''
+        return norm.pdf(self._d1_) / (self.spot * self._a_)
+
+    # Option Vega
+    @property
+    def _vega(self):
+        '''Returns the option vega'''
+        if self.volatility == 0 or self.dte == 0:
+            return 0.0
+        else:
+            return self.spot * norm.pdf(self._d1_) * self.dte**0.5 / 100
+
+    # Option Theta
+    @property
+    def _theta(self):
+        '''Returns the option theta: [Call theta, Put theta]'''
+        call = -self.spot * norm.pdf(self._d1_) * self.volatility / (2 * self.dte**0.5) - self.rate * self.strike * self._b_ * norm.cdf(self._d2_)
+
+        put = -self.spot * norm.pdf(self._d1_) * self.volatility / (2 * self.dte**0.5) + self.rate * self.strike * self._b_ * norm.cdf(-self._d2_)
+        return [call / 365, put / 365]
+
+    # Option Rho
+    @property
+    def _rho(self):
+        '''Returns the option rho: [Call rho, Put rho]'''
+        call = self.strike * self.dte * self._b_ * norm.cdf(self._d2_) / 100
+        put = -self.strike * self.dte * self._b_ * norm.cdf(-self._d2_) / 100
+
+        return [call, put]
+    
+    # Implied volatility
+    @property
+    def _impvol(self):
+        '''Derive the implied volatility'''
+        if (self.callprice or self.putprice) is None:
+            return self.volatility
+        else:
+            # Bisection method
+            def bisection_iv(high=500., low=0., tolerance=1e-7):
+                
+                for i in range(1000):
+                    mid = (high+low)/2
+                    if mid < tolerance:
+                        mid = tolerance
+                        
+                    option = BS(self.spot, self.strike, self.rate, self.dte, mid) # mid is sigma here
+                    
+                    if self.callprice:
+                        price = self.callprice
+                        estimate = option.callPrice # BS price
+                        
+                    if self.putprice:
+                        price = self.putprice
+                        estimate = option.putPrice # BS price
+                        
+                    
+                    if round(estimate,6) == price:
+                        break
+                    elif estimate > price:
+                        high = mid
+                    elif estimate < price:
+                        low = mid
+                        
+                return mid
+        return bisection_iv()
